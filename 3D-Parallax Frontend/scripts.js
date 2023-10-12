9398811f0ebbd835650e84df3407be315e0da6fd
hector <hectormotazorrilla@gmail.com>
2023-10-12 15:20:49 -0400

    3d corregido

--- 3D-Parallax Frontend/scripts.js
+++ 3D-Parallax Frontend/scripts.js
@@ -2,38 +2,81 @@ document.addEventListener('mousemove', function (e) {
     var wx = window.innerWidth;
     var wy = window.innerHeight;
 
-    var x = e.pageX - window.scrollX;
-    var y = e.pageY - window.scrollY;
+    // Calcula el centro de la pantalla
+    var centerX = wx / 2;
+    var centerY = wy / 2;
 
-    var newx = x - wx ;
-    var newy = y - wy ;
+    // Calcula las coordenadas ajustadas al centro
+    var x = e.pageX - window.scrollX - centerX;
+    var y = e.pageY - window.scrollY - centerY;
+
+    var newx = x - wx;
+    var newy = y - wy;
+
+    // Calcula las coordenadas en grados
+    var xdeg = 20 * (x) / (centerX);
+    var ydeg = 50 * (y) / (centerY);
+
+    // Obtener el ancho y alto del wrapper
+    var wrapper = document.getElementById('wrapper');
+    var wrapperX = wrapper.offsetWidth;
+    var wrapperY = wrapper.offsetHeight;
+
+    // Verifica si el ratón está cerca de los bordes del contenedor (20 píxeles de margen)
+    var margin = 20;
+    var nearWrapperX = Math.abs(x) < (wrapperX / 2) + margin;
+    var nearWrapperY = Math.abs(y) < (wrapperY / 2) + margin;
 
     document.querySelectorAll('#wrapper .p1, #wrapper .p2, #wrapper .p3, #wrapper .letra').forEach(function (element) {
         var speed = parseFloat(element.getAttribute('data-speed')) || 0;
         element.style.transform = 'translate(' + (1 - newx * speed) + 'px, ' + (1 - newy * speed) + 'px)';
     });
+
+    document.querySelectorAll('#container').forEach(function (element) {
+        var speed = parseFloat(element.getAttribute('data-speed')) || 0;
+        var transitionTime = '0.3s'; // Ajusta la duración de la transición según sea necesario
+        element.style.transition = `transform ${transitionTime} ease-out`;
+
+        // Aplica transformaciones solo si está cerca del borde del wrapper
+        if (nearWrapperX && nearWrapperY) {
+            element.style.transform = 'perspective(1000px) rotateX(' + -ydeg + 'deg) rotateY(' + xdeg + 'deg)';
+            document.querySelectorAll('#wrapper').forEach(function (element) {
+                element.style.transform = 'translate('+'-50%, -50%) scale(1.1)';
+                element.style['box-shadow'] = ' 0px 0px 10px 10px rgba(0, 0, 0, 0.5)';
+                element.style.transition = `0.3s ease-out`;
+            });
+        } else {
+            element.style.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg)';
+            document.querySelectorAll('#wrapper').forEach(function (element) {
+                element.style.transform = 'translate('+'-50%, -50%) scale(1)';
+                element.style['box-shadow'] = ' 0px 0px 0px 0px rgba(0, 0, 0, 0)';
+                element.style.transition = `0.3s ease-out`;
+            });
+        }
+    });
+
+
+
+    console.log('wx:', wx);
+    console.log('wy:', wy);
+    console.log('x:', x);
+    console.log('y:', y);
+    console.log('xdeg:', xdeg);
+    console.log('ydeg:', ydeg);
+    console.log('WrapperX:', wrapperX);
+    console.log('WrapperY:', wrapperY);
+    console.log('nearWrapperX:', nearWrapperX);
+    console.log('nearWrapperY:', nearWrapperY);
+
+
+
+
+
+
+
+
+
+
+
 });
 
-
-// codigo con jquery y gsap
-// $('html').mousemove(function(e){
-		
-// 		var wx = $(window).width();
-// 		var wy = $(window).height();
-		
-// 		var x = e.pageX - this.offsetLeft;
-// 		var y = e.pageY - this.offsetTop;
-		
-// 		var newx = x - wx/2;
-// 		var newy = y - wy/2;
-		
-// 		$('span').text(newx + ", " + newy);
-		
-// 		$('#wrapper div').each(function(){
-// 			var speed = $(this).attr('data-speed');
-// 			if($(this).attr('data-revert')) speed *= -1;
-// 			TweenMax.to($(this), 1, {x: (1 - newx*speed), y: (1 - newy*speed)});
-			
-// 		});
-		
-// 	});
